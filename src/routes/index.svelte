6f3a44371cb85dfb7c0a610cffaa383066fea963
Vivek Durai <1385776+vivekdurai@users.noreply.github.com>
2022-07-25 02:16:37 +0530

    blank page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mb-20 space-y-20 overflow-hidden sm:mb-32 sm:space-y-32 md:mb-40 md:space-y-40">
+	<div class="px-4 sm:px-6 md:px-8">
+		<div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+			<h1
+				class="text-slate-900 font-sans font-extrabold text-5xl sm:text-6xl lg:text-7xl lg:tracking-relaxed sm:tracking-tight dark:leading-relaxed sm:leading-tight text-center dark:text-black"
+			>
+				Accelerate your transactions with document intelligence.
+			</h1>
+			<p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
+				Use our AI powered document platform to power <span
+					class="font-mono font-medium text-red-500 dark:text-red-400">insights</span
+				>, <span class="font-mono font-medium text-red-500 dark:text-red-400">workflows</span> and
+				<span class="font-mono font-medium text-red-500 dark:text-red-400">dashboards</span> across your
+				organization.
+			</p>
+			<div class="mt-8 sm:mt-7 flex justify-center space-x-6 text-sm">
+				<a
+					class="bg-orange-900 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-orange-500 dark:highlight-white/20 dark:hover:bg-orange-600"
+					href="/docs/installation">Request Access</a
+				>
+			</div>
+		</div>
+	</div>
+	<div
+		class="pt-10 mb-10 space-y-10 overflow-hidden sm:pt-12 sm:mb-12 sm:space-y-32 md:pt-10 md:mb-10 md:space-y-10"
+	>
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+			<h2 class="mt-8 font-semibold text-indigo-500 dark:text-indigo-400">Developer-first</h2>
+			<p
+				class="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight dark:text-black-50 "
+			>
+				An API for your Documents.
+			</p>
+		</div>
+	</div>
+	/>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			fontFamily: {
+				serif: ['Oregano', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif']
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
