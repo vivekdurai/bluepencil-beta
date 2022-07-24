9a9d1498ed4edc68f683a8004e0e50c70023dc08
Vivek Durai <1385776+vivekdurai@users.noreply.github.com>
2022-07-25 03:01:44 +0530

    blank page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
 			</p>
 			<div class="mt-8 sm:mt-7 flex justify-center space-x-6 text-sm">
 				<a
-					class="text-lg bg-sky-900 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-14 px-10 rounded-md w-full flex items-center justify-center sm:w-auto py-6 dark:bg-sky-900 dark:highlight-white/20 dark:hover:bg-sky-400"
+					class="text-lg bg-sky-700 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-orange-600 border-orange-100 font-semibold h-14 px-10 rounded-md w-full flex items-center justify-center sm:w-auto py-6 dark:bg-sky-500  hover:bg-gradient-to-tr bg-gradient-to-r from-whie-300  to-white-300 border-2 border-solid  to-white-300 dark:highlight-white/20 dark:hover:bg-sky-400"
 					href="/docs/installation">Schedule a call</a
 				>
 			</div>
