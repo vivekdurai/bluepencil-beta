8d1f54836cd2be1e66a763b482cccab8ebe32ce8
Vivek Durai <1385776+vivekdurai@users.noreply.github.com>
2022-07-25 11:00:15 +0530

    blank page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,3 @@
 </script>
 
 <slot />
-
-<style global lang="postcss">
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
-</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,15 @@
+<script>
+	import Nav from '../lib/Nav.svelte/';
+	import Hero from '../lib/Hero.svelte';
+</script>
+
+<svelte:head>
+	<title>OneBigPicture: Powering Document Intelligence across your organization</title>
+</svelte:head>
+
+<Nav />
 <div class="mb-20 space-y-20 overflow-hidden sm:mb-32 sm:space-y-32 md:mb-40 md:space-y-40">
-	<div class="px-4 sm:px-6 md:px-8">
-		<div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-			<h1
-				class="text-slate-900 font-sans font-extrabold text-5xl sm:text-6xl lg:text-7xl lg:tracking-relaxed sm:tracking-tight dark:leading-relaxed sm:leading-tight text-center dark:text-black"
-			>
-				Accelerate your transactions with document intelligence.
-			</h1>
-			<p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
-				Use our AI powered document platform to power <span
-					class="font-mono font-medium text-sky-900 dark:text-sky-900">insights</span
-				>, <span class="font-mono font-medium text-sky-900 dark:text-sky-900">workflows</span>
-				and
-				<span class="font-mono font-medium text-sky-900 dark:text-sky-900">dashboards</span> across your
-				organization.
-			</p>
-			<div class="mt-8 sm:mt-7 flex justify-center space-x-6 text-sm">
-				<a
-					class="text-lg focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-orange-600 border-orange-200 font-semibold h-14 px-10 rounded-md w-full flex items-center justify-center sm:w-auto py-6  hover:bg-gradient-to-tr bg-gradient-to-r from-transparent  to-transparent border-2 border-solid  to-white-300 dark:highlight-white/20 dark:hover:border-black-300 dark:hover:bg-orange-100"
-					href="/docs/installation">Schedule a call</a
-				>
-			</div>
-		</div>
-	</div>
+	<Hero />
 	<div
 		class="pt-10 mb-10 space-y-10 overflow-hidden sm:pt-12 sm:mb-12 sm:space-y-32 md:pt-10 md:mb-10 md:space-y-10"
 	>
@@ -34,5 +22,4 @@
 			</p>
 		</div>
 	</div>
-	/>
 </div>
