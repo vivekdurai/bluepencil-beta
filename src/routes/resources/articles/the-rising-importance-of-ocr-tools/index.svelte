bb56a8bfd73e6d3ac14dda0088d4bfc6ea38b39c
Vivek Durai <1385776+vivekdurai@users.noreply.github.com>
2022-09-23 10:58:31 +0530

    added doc.png

---
+++ src/routes/resources/articles/the-rising-importance-of-ocr-tools/index.svelte
@@ -0,0 +1,44 @@
+<script>
+	import SvelteSeo from "svelte-seo";
+</script>
+<SvelteSeo
+  title="One by OneBigPicture: The Rising Importance of OCR Tools"
+  description="Accelerate sales, buying, hiring and other critical business functions with an intelligent document platform designed for humans"
+  twitter={{
+    site: "@onebigpicture",
+    title: "One by OneBigPicture: The Rising Importance of OCR Tools",
+    description: "Accelerate business with an intelligent document platform designed for humans",
+    image: "https://www.obp.one/obp_one_preview.png",
+    imageAlt: "One: designed for humans",
+  }}
+  openGraph={{
+    title: 'One by OneBigPicture: The Rising Importance of OCR Tools',
+    description: 'Accelerate sales, buying, hiring and other critical business functions with an intelligent document platform designed for humans',
+    url: 'https://www.obp.one',
+    type: 'website',
+    images: [
+      {
+        url: "https://www.obp.one/obp_one_preview.png",
+        width: 909,
+        height: 90,
+        alt: 'One by OneBigPicture'
+      }
+     ]
+  }}
+/>
+<h1>The Importance of OCR Tools</h1>
+<p>As we increasingly rely on digital information, the ability to convert images and PDFs into text becomes more and more important. OCR (optical character recognition) tools are essential for extracting text from these sources, and there are a number of different options available. In this article, we'll take a look at some of the best OCR tools on the market and discuss why they're so important.</p>
+<h2>What is OCR?</h2>
+<p>Optical character recognition (OCR) is the process of converting scanned images of text into machine-readable text. OCR tools allow you to take a scanned document or image and extract the text from it so that you can edit, search, or otherwise manipulate the text.<br><br>There are a number of reasons why you might want to use an OCR tool. For instance, you may need to convert a scanned document into a digital format so that you can edit it or search for specific information within the document. Or, you may want to digitize a paper document so that you can share it electronically with others.<br><br>Whatever your reason for needing an OCR tool, there are a few things to keep in mind when choosing one. First, consider whether you need a tool that can handle multiple languages. Many OCR tools can only recognize English text, but some can handle other languages as well.<br><br>Next, think about the accuracy of the OCR tool. The best tools will be able to accurately convert images of text into editable text. However, not all OCR tools are created equal in this respect, so it’s important to choose one that is known for its accuracy.<br><br>Finally, consider</p>
+<h2>The Benefits of OCR Tools</h2>
+<p>There are many reasons why you should consider using an OCR tool for your business or personal needs. Here are just a few benefits of using OCR tools:<br><br>1. Improve Productivity and Efficiency<br><br>OCR tools can help you work faster and more efficiently. With OCR, you can scan and convert paper documents into digital files that can be edited, searched, and shared easily. This can save you a lot of time and hassle, especially if you have to deal with a lot of paper documents on a daily basis.<br><br>2. Reduce Costs<br><br>Another great benefit of OCR tools is that they can help you reduce costs. For businesses, OCR can help them cut down on paper usage and storage costs. For individuals, OCR can help you save on the cost of printing documents.<br><br>3. Enhance Accessibility and Usability<br><br>OCR tools can also enhance the accessibility and usability of your documents. With OCR, you can make your documents accessible to people with disabilities or those who prefer to read digital content. You can also make your documents more user-friendly by making them searchable and shareable online.</p>
+<h2>The Different Types of OCR Tools</h2>
+<p>There are a few different types of OCR tools available on the market today. Here is a quick overview of some of the most popular options:<br><br>1. Desktop OCR Software: This type of software is installed on your computer and can be used to convert scanned documents or images into editable text files. Some of the most popular desktop OCR software programs include OmniPage, Abbyy FineReader, and ReadIris.<br><br>2. Online OCR Tools: These tools allow you to convert scanned documents or images into editable text files without having to install any software on your computer. Some of the most popular online OCR tools include FreeOCR, Online OCR, and Convertio.<br><br>3. Mobile OCR Apps: These apps allow you to convert scanned documents or images into editable text files using your smartphone or tablet. Some of the most popular mobile OCR apps include Text Fairy, CamScanner, and Office Lens.</p>
+<h2>How to Use OCR Tools</h2>
+<p>OCR tools are important for anyone who needs to convert images of text into editable text. While there are many different OCR tools available, they all work in essentially the same way. Here's a quick guide to using OCR tools to convert images into text.<br><br>First, you'll need to select an OCR tool. There are many different options available, so take some time to compare features and prices to find the best tool for your needs. Once you've selected a tool, download it and install it on your computer.<br><br>Next, open the image that you want to convert into text. The image can be in any format, but JPEG and PNG files are the most common. Select the portion of the image that contains the text you want to convert, and then copy it to the clipboard.<br><br>Now, open the OCR tool that you installed earlier. In most cases, you'll simply need to paste the image from the clipboard into the program. The tool will then analyze the image and attempt to convert it into text.<br><br>Once the conversion is complete, you can save the text file for later use. That's all there is to using OCR tools! With just a few clicks, you</p>
+<h2>The Best OCR Tools</h2>
+<p>As we all know, Optical Character Recognition (OCR) is the process of converts scanned images of text into editable text. This is an important tool because it allows us to edit and manipulate text that would otherwise be uneditable.<br><br>There are many different OCR tools available on the market, but which one is the best? In this blog post, we will take a look at the best OCR tools and what makes them stand out from the rest.<br><br>Top OCR Tools<br><br>1. Adobe Acrobat DC <br>2. Abbyy FineReader <br>3. Nuance Power PDF <br>4. IRIS Readiris <br>5. Cisdem PDFCompressor <br><br>Adobe Acrobat DC is one of the most popular PDF editors and it comes with a powerful OCR tool that can easily convert scanned images to editable text. The software is easy to use and it offers a wide range of features. However, it is quite expensive and it doesn’t offer a free trial. <br><br>Abbyy FineReader is another popular OCR tool that offers an accurate and fast conversion of scanned images to editable text. It also comes with a wide range</p>
+<h2>Alternatives to OCR Tools</h2>
+<p>There are a few alternatives to OCR tools that can be used for text recognition. One such alternative is online services that offer OCR capabilities. These services can be used to convert images of text into digital text that can be edited, searched, and stored more easily.<br><br>Another alternative to OCR tools is optical character recognition software. This type of software can be installed on a computer and used to recognize text in images. Some optical character recognition software is free, while others must be purchased.<br><br>A third alternative to OCR tools is human transcription services. These services hire people to manually transcribe text from images. This option may be more accurate than OCR, but it is also more time-consuming and expensive.</p>
+<h2>Conclusion</h2>
+<p>As you can see, OCR tools are extremely important in today's business world. They can help you save time and money by automating tasks that would otherwise be done manually. Not only that, but they can also help you improve the quality of your work by reducing errors. If you're not already using an OCR tool, we highly recommend that you start doing so as soon as possible.</p>
